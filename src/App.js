a71b043955c610a7526ef9526fd8a2b22a2052fb
SilvijaSvegzde <83742823+SilvijaSvegzde@users.noreply.github.com>
2021-10-13 16:51:12 +1100

    added axios

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header"> Dictionary </header>
+        <br />
         <main>
           <Dictionary />
         </main>
+        <br />
         <footer className="text-center">Coded by Silvija Svegzde</footer>
       </div>
     </div>
